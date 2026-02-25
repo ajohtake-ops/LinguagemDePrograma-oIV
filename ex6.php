c25eabe2af73d533d5f02addd799a4de89cbe180
Adrian Takeuchi <ajoh.take@gmail.com>
2026-02-25 18:58:20 -0300

    foi feito o exercício 6

---
+++ ex6.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Exercício 6</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container py-3">
+        <h1>Exercício 6</h1>
+        <form method="post">
+            <div class="mb-3">
+                <label for="nota1" class="form-label">Insira o valor da temperatura em Celsius:</label>
+                <input type="number" id="nota1" name="tempCelsius" class="form-control" required="">
+            </div>
+            <button type="submit" class="btn btn-primary">Enviar</button>
+        </form>
+        <?php
+
+            if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+                $tempCelsius = $_POST['tempCelsius'];
+
+                $tempF = ($tempCelsius*1.8) + 32;
+
+                echo "A temperatura em Fahrenheit é de: $tempF";
+            }
+
+        ?>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+    </div>
+</body>
+
+</html>
