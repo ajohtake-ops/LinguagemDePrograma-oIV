14934cd042edf0894aa1770f1dd66aecf8e17a4d
Adrian Takeuchi <ajoh.take@gmail.com>
2026-03-02 22:03:44 -0300

    foram feitos os exercícios 13 ao 15

---
+++ ex15.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>exercício 15</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container py-3">
+        <h1>exercício 15</h1>
+        <form method="post">
+            <div class="mb-3">
+                <label for="peso" class="form-label">Insira o valor do seu peso:</label>
+                <input type="number" step="any" id="peso " name="peso" class="form-control" required="">
+                <label for="altura" class="form-label">Insira o valor da sua altura:</label>
+                <input type="number" step="any" id="altura" name="altura" class="form-control" required="">
+            </div>
+            <button type="submit" class="btn btn-primary">Enviar</button>
+        </form>
+        <?php
+
+        if ($_SERVER['REQUEST_METHOD'] = 'POST') {
+            $peso = $_POST['peso'];
+            $altura = $_POST['altura'];
+            $resultado = $peso / ($altura ** 2);
+            echo "O resultado do seu IMC é: $resultado.";
+
+            if ($resultado < 18.5) {
+                echo "\nVocê está abaixo do peso.";
+            } elseif ($resultado >= 18.5 & $resultado < 25) {
+                echo "\nVocẽ está no peso adequado";
+            } elseif ($resultado >= 25) {
+                echo "\nVocê está acima do peso.";
+            }
+        }
+
+        ?>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+    </div>
+</body>
+
+</html>
