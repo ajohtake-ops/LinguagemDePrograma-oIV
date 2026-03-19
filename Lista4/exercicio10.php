88a9ea649766d22ff3d12f52d3f13f5c597cae47
Adrian Takeuchi <ajoh.take@gmail.com>
2026-03-19 19:29:36 -0300

    lista 4:
    exercícios 4 ao 10

---
+++ Lista4/exercicio10.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Exercício 10 PHP</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container py-3">
+        <h1>Exercício 10 PHP</h1>
+        <form method="post">
+            <div class="mb-3">
+                <label for="nome" class="form-label">Digite um nome completo (apenas as iniciais em maiúsculo):</label>
+                <input type="text" id="nome" name="nome" class="form-control" required="">
+            </div>
+            <button type="submit" class="btn btn-primary">Enviar</button>
+        </form>
+        <?php
+
+        if ($_SERVER['REQUEST_METHOD'] = 'POST') {
+            $nome = $_POST['nome'];
+
+            $iniciais = str_replace(range('a','z'),'',$nome);
+
+            $sigla = str_replace(' ', '.', $iniciais);
+
+            echo "<br>Nome: $nome</br>";
+            echo "<br>Sigla do nome: $sigla</br>";
+
+        }
+        ?>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+    </div>
+</body>
+
+</html>
---
+++ Lista4/exercicio4.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Exercício 4</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container py-3">
+        <h1>Exercício 4</h1>
+        <form method="post">
+            <div class="mb-3">
+                <label for="dia" class="form-label">Dia:</label>
+                <input type="number" id="dia" name="dia" class="form-control" required="">
+            </div>
+            <div class="mb-3">
+                <label for="mes" class="form-label">Mês</label>
+                <input type="number" id="mes" name="mes" class="form-control" required="">
+            </div>
+            <div class="mb-3">
+                <label for="ano" class="form-label">Ano:</label>
+                <input type="number" id="ano" name="ano" class="form-control" required="">
+            </div>
+            <button type="submit" class="btn btn-primary">Enviar</button>
+        </form>
+        <?php
+
+        if ($_SERVER['REQUEST_METHOD'] = 'POST') {
+            $dia = $_POST['dia'];
+            $mes = $_POST['mes'];
+            $ano = $_POST['ano'];
+
+            if (checkdate($mes, $dia, $ano)) {
+                echo "<br>A data inserida é válida.</br>";
+                echo sprintf('%02d', $dia), "/";
+                echo sprintf('%02d', $mes), "/";
+                echo sprintf('%04d', $ano);
+            }else{
+                echo "<br>A data inserida não é válida.</br>";
+            }
+        }
+
+        ?>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+    </div>
+</body>
+
+</html>
